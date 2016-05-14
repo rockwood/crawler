b09e3a657c72cc7cd984f619cebaa001dea5fc08
Kevin Rockwood <kevin@rockwood.me>
2016-05-14 14:21:12 -0400

    Update http adapter to only fetch valid pages

--- lib/crawler/adapters/http.ex
+++ lib/crawler/adapters/http.ex
@@ -1,6 +1,22 @@
 defmodule Crawler.HttpAdapter do
+  alias Crawler.Page
+
   def get(page) do
-    response = HTTPoison.get!(page.url)
-    %{page | body: response.body}
+    if Page.valid?(page) do
+      case make_request(page) do
+        {:ok, response} ->
+          %{page | body: response.body}
+        {:error, _} ->
+          page
+      end
+    else
+      page
+    end
+  end
+
+  def make_request(page) do
+    page.uri
+    |> URI.to_string
+    |> HTTPoison.get
   end
 end
