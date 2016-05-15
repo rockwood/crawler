ca797fb3d1cfd723094c51b808525359ec034c8f
Kevin Rockwood <kevin@rockwood.me>
2016-05-15 19:12:31 -0400

    Convert front-end to new server api

--- web/static/js/graph.js
+++ web/static/js/graph.js
@@ -1,58 +1,78 @@
 const d3 = window.d3;
 const $ = window.jQuery;
 
-const COLOR = d3.scale.category20();
+const color = d3.scale.category20();
 
 export default class Graph {
   constructor() {
     const WIDTH = $("body").width();
-    const HEIGHT = 1000;
-
-    this.layout = d3.layout.force()
-      .charge(-120)
-      .linkDistance(30)
-      .size([WIDTH, HEIGHT]);
+    const HEIGHT = $("body").height();
 
     this.svg = d3.select("body")
       .append("svg")
       .attr("width", WIDTH)
       .attr("height", HEIGHT);
+
+    this.nodes = [];
+    this.links = [];
+
+    this.layout = d3.layout.force()
+      .nodes(this.nodes)
+      .links(this.links)
+      .charge(-120)
+      .linkDistance(60)
+      .size([WIDTH, HEIGHT]);
+
+    this.layout.on("tick", this.tick.bind(this));
   }
 
-  draw(payload) {
-    this.layout
-      .nodes(payload.nodes)
-      .links(payload.links)
-      .start();
+  drawPage(page) {
+    console.log(page);
+    this.nodes.push({ id: page.id, index: page.id, uri: page.uri });
+    this.render();
+  }
 
+  drawLink(link) {
+    console.log(link);
+    this.links.push(link);
+    this.render();
+  }
+
+  render() {
     let link = this.svg.selectAll(".link")
-      .data(payload.links)
-      .enter().append("line")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+      .data(this.layout.links());
 
-    let node = this.svg
-      .selectAll(".node")
-      .data(payload.nodes)
-      .enter().append("circle")
+    link
+      .enter()
+      .insert("line", ".node")
+      .attr("class", "link");
+
+    link.exit().remove();
+
+    let node = this.svg.selectAll(".node")
+      .data(this.layout.nodes(), function(d) { return d.id; });
+
+    node.enter()
+      .append("circle")
+      .attr("class", "circle")
       .attr("class", "node")
-      .attr("r", 5)
-      .style("fill", function(d) { return COLOR(d.group); })
-      .call(this.layout.drag);
+      .attr("r", 10)
+      .style("fill", function(d) { return color(d.uri.host); });
 
-    node
-      .append("title")
-      .text(function(d) { return d.name; });
+    node.exit().remove();
 
-    this.layout.on("tick", function() {
-      link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+    this.layout.start();
+  };
 
-      node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-    });
-  }
+  tick() {
+    this.svg.selectAll(".link")
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+    this.svg.selectAll(".node")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+  };
 }
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -4,7 +4,7 @@ import Graph from "./graph";
 let socket = new Socket("/socket", {params: {token: window.userToken}});
 socket.connect();
 
-let channel = socket.channel("urls:test_url", {});
+let channel = socket.channel("crawler:main", {});
 
 let graph = new Graph();
 
@@ -12,8 +12,17 @@ channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp); })
   .receive("error", resp => { console.log("Unable to join", resp); });
 
-channel.on("update", (payload) => {
-  graph.draw(payload);
+channel.on("new_page", (page) => {
+  graph.drawPage(page);
+});
+
+channel.on("new_link", (link) => {
+  graph.drawLink(link);
+});
+
+$("form").submit((evt) => {
+  evt.preventDefault();
+  channel.push("crawl", {url: $("input").val()});
 });
 
 export default socket;
