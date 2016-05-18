0c7bdab0262fdf8c7083cb7c30847ab733e9004c
Kevin Rockwood <kevin@rockwood.me>
2016-05-18 17:00:28 -0500

    Update CrawlerChannel with new Crawler api

--- apps/objects/web/channels/crawler_channel.ex
+++ apps/objects/web/channels/crawler_channel.ex
@@ -2,13 +2,11 @@ defmodule Objects.CrawlerChannel do
   use Phoenix.Channel
 
   def join("crawler:main", _message, socket) do
-    send(self, :after_join)
-
     {:ok, socket}
   end
 
-  def handle_info(:after_join, socket) do
-    Crawler.on_update fn(update) ->
+  def handle_in("crawl", %{"url" => url}, socket) do
+    Crawler.start_link url, fn(update) ->
       case update do
         {:page, new_page} ->
           push(socket, "new_page", Map.take(new_page, [:id, :uri]))
@@ -16,11 +14,7 @@ defmodule Objects.CrawlerChannel do
           push(socket, "new_link", new_link)
       end
     end
-    {:noreply, socket}
-  end
 
-  def handle_in("crawl", %{"url" => url}, socket) do
-    Crawler.run(url)
     {:noreply, socket}
   end
 end
